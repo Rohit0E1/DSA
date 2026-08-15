d8acac8bec2e313fc70f48932409a69989197797
Rohit0E1 <kumarrohit.2512003@gmail.com>
2026-08-15 23:57:28 +0530

    Time: 0 ms (100%), Space: 54.8 MB (65.79%) - LeetHub

---
+++ 0078-subsets/0078-subsets.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var subsets = function(nums) {
+    let res = [];
+
+    const backtrack =(path, start)=>{
+        res.push([...path])
+        for(let i = start; i < nums.length; i++){
+            path.push(nums[i]);
+            backtrack(path, i+1);
+            path.pop();
+        }
+    }
+
+    backtrack([],0);
+    return res;
+};
