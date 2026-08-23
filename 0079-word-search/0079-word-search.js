64b529e5ac94b4b82aa1ce64ae79ce7a65c75999
Rohit0E1 <kumarrohit.2512003@gmail.com>
2026-08-23 21:18:00 +0530

    Time: 378 ms (32.42%), Space: 57.3 MB (29.68%) - LeetHub

--- 0079-word-search/0079-word-search.js
+++ 0079-word-search/0079-word-search.js
@@ -7,39 +7,32 @@ var exist = function (board, word) {
     let res = false;
 
 
-    const wordMatcher = (row, col, wordi, map) => {
+    const wordMatcher = (row, col, wordi) => {
         if (word.length == wordi) {
             console.log(word, wordi)
             res = true;
             return;
         }
-        if ( !map.has(String(row+1) + String(col)) && board[row + 1] && board[row + 1][col] == word[wordi]) {
-            map.set(String(row+1) + String(col));
-            wordMatcher(row + 1, col, wordi + 1, map);
-            map.delete(String(row+1) + String(col))
+        let temp = board[row][col];
+        board[row][col] = "#"
+        if (board[row + 1] && board[row + 1][col] == word[wordi]) {
+            wordMatcher(row + 1, col, wordi + 1);
         }
-        if ( !map.has(String(row-1) + String(col)) && board[row - 1] && board[row - 1][col] == word[wordi]) {
-            map.set(String(row-1) + String(col));
-            wordMatcher(row - 1, col, wordi + 1, map);
-            map.delete(String(row-1) + String(col))
+        if (board[row - 1] && board[row - 1][col] == word[wordi]) {
+            wordMatcher(row - 1, col, wordi + 1);
         }
-        if ( !map.has(String(row) + String(col+1)) && board[row][col + 1] && board[row][col + 1] == word[wordi]) {
-            map.set(String(row) + String(col+1));
-            wordMatcher(row, col + 1, wordi + 1, map);
-            map.delete(String(row) + String(col+1))        
+        if ( board[row][col + 1] && board[row][col + 1] == word[wordi]) {
+            wordMatcher(row, col + 1, wordi + 1);        
         }
-        if ( !map.has(String(row) + String(col-1)) &&board[row][col - 1] && board[row][col - 1] == word[wordi]) {
-            map.set(String(row) + String(col-1));
-            wordMatcher(row, col - 1, wordi + 1, map);
-            map.delete(String(row) + String(col-1))
+        if (board[row][col - 1] && board[row][col - 1] == word[wordi]) {
+            wordMatcher(row, col - 1, wordi + 1);
         }
+        board[row][col] = temp;
     }
     for (let i = 0; i < board.length; i++) {
         for (let j = 0; j < board[i].length; j++) {
             if (word[0] == board[i][j]) {
-                let map = new Map();
-                map.set(String(i) + String(j));
-                wordMatcher(i, j, 1, map);
+                wordMatcher(i, j, 1);
             }
         }
     };
