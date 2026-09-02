8457adc2aaeb25b0a98d6cc96821fc00106dd7a7
Rohit0E1 <kumarrohit.2512003@gmail.com>
2026-09-02 23:50:15 +0530

    Time: 742 ms (5.06%), Space: 84.3 MB (96.9%) - LeetHub

---
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[][]} intervals
+ * @return {number}
+ */
+var eraseOverlapIntervals = function (int) {
+    int.sort((a, b) => a[1] - b[1]);
+    console.log(int)
+    let res = 0;
+
+    for (let i = 1; i < int.length; i++) {
+        console.log(int[i-1][1], int[i][0], int[i-1][1] > int[i][0])
+        if(int[i-1][1] > int[i][0]) {
+            res++ 
+            int[i][1] = Math.min(int[i-1][1],int[i][1]);
+        };
+    }
+
+    return res;
+};
