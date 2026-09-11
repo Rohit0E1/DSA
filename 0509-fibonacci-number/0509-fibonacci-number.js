4f806cdf58a3935e7a8a45d82560e4219155188c
Rohit0E1 <kumarrohit.2512003@gmail.com>
2026-09-11 11:41:21 +0530

    Time: 44 ms (72.87%), Space: 53.8 MB (34.78%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.js
+++ 0509-fibonacci-number/0509-fibonacci-number.js
@@ -2,8 +2,16 @@
  * @param {number} n
  * @return {number}
  */
-var fib = function(n) {
-    if(n < 2) return n;
+var fib = function (n) {
+    let map = new Map();
+    const fib1 = function (n) {
+        if (n < 2) return n;
+        if(map.has(n)) return map.get(n)
+        let man = fib1(n - 1) + fib1(n - 2)
+        if(!map.has(n)) map.set(n,man)
 
-    return fib(n-1) + fib(n-2)
-};
+        return map.get(n);
+    };
+
+   return fib1(n)
+}
